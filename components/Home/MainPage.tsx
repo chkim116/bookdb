214db4ae5e4f619bed7e169eabf2882312eb37f5
chkim116 <dudnqnfqlc@naver.com>
2020-12-03 06:36:53 +0900

    Feat Gyobo bestseller crawling for MainPage

--- components/Home/MainPage.tsx
+++ components/Home/MainPage.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Link from "next/link";
+import { BestSeller } from "../../pages";
+import { Button } from "../../styles/CommonStyle";
 
-const MainPage = () => {
-    return <div></div>;
+const MainPage = ({ list }: any) => {
+    return (
+        <div>
+            {list.map((v: BestSeller) => (
+                <div key={v.id}>
+                    <div>{v.title}</div>
+                    <div>{v.auth}</div>
+                    <div>{v.summary}</div>
+                    <div>
+                        <img src={v.imageUrl} alt={v.imageAlt} />
+                    </div>
+                    <Link href={v.url}>
+                        <a target="blank">
+                            <Button>더보기</Button>
+                        </a>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default MainPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,61 @@
 import styled from "@emotion/styled";
 import MainPage from "../components/Home/MainPage";
+import Axios from "axios";
+import cheerio from "cheerio";
+import { GetStaticProps } from "next";
+import iconv from "iconv-lite";
 
 const Container = styled.div`
     width: 100%;
 `;
 
-export default function Home() {
+export type BestSeller = {
+    title: string;
+    auth: string;
+    imageAlt: string;
+    imageUrl: string;
+    summary: string;
+    url: string;
+    id: number;
+};
+
+export default function Home({ list }: any) {
     return (
         <Container>
-            <MainPage></MainPage>;
+            <MainPage list={list} />
+            <div></div>
         </Container>
     );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    let list: BestSeller[] = [];
+    try {
+        const gyobo = await Axios.get(
+            "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?orderClick=d80",
+            { responseType: "arraybuffer" }
+        ).then((res) => res.data);
+        const decoded = iconv.decode(gyobo, "EUC-KR");
+        const $ = cheerio.load(decoded);
+        const $bodyList = $("ul.list_type01").children("li");
+
+        $bodyList.each(function (i) {
+            list[i] = {
+                title: $(this).find("div.title strong").text(),
+                url: $(this).find("div.cover a").attr("href"),
+                imageUrl: $(this).find("img").attr("src"),
+                imageAlt: $(this).find("img").attr("alt"),
+                summary: $(this).find("div.subtitle").text(),
+                auth: $(this).find("div.author").text(),
+                id: i++,
+            };
+        });
+    } catch (err) {
+        console.log(err);
+    }
+    return {
+        props: {
+            list,
+        },
+    };
+};
